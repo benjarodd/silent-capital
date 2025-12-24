08ec3981d5f3edf29aaa9af0e9ba8002ad27e3d1
benjarodd <xosjggg713@outlook.com>
2025-12-24 18:03:47 +0800

    Hook up deploy script for FHEETH + SilentCapital

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedFHEETH = await deploy("FHEETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedSilentCapital = await deploy("SilentCapital", {
+    from: deployer,
+    log: true,
+    args: [deployedFHEETH.address],
+  });
+
+  console.log(`FHEETH contract: `, deployedFHEETH.address);
+  console.log(`SilentCapital contract: `, deployedSilentCapital.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_silent_capital"; // id required to prevent reexecution
+func.tags = ["SilentCapital"];
